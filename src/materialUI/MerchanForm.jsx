6c79ed599deafc8e3c8a867ba14e6837c48b0533
Neha <86464289+nehay2904@users.noreply.github.com>
2023-02-05 13:52:11 +0530

    Add files via upload

---
+++ src/materialUI/MerchanForm.jsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Paper } from '@mui/material';
+
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+import emailjs from '@emailjs/browser';
+import { Link } from 'react-router-dom';
+
+export default function MerchandForm() {
+
+ 
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_7wfwqo5', 'template_swu0mhz', form.current, 'TqO-LjB1jDL7sr4EH')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent")
+      }, (error) => {
+          console.log(error.text);
+          console.log("message not sent")
+      });
+  };
+
+
+
+
+  return (
+  <>
+   
+   <Paper elevation={4} className='form_paper_merchan'>
+      <div>
+      <Box
+        className='formwidthmerchan'
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1 },
+        }}
+        noValidate
+        autoComplete="off"
+        ref={form} 
+      >
+       
+       <TextField id="standard-basic" name='user_name' label="Name" variant="standard" />
+        {/* <TextField id="standard-basic" name='user_contact' label="Contact No" variant="standard" /> */}
+        <TextField id="standard-basic" name='user_email' label="email" variant="standard" />
+        <TextField id="standard-basic" name='user_size' label="Size for merchandise (XS, S, M , L XL)" variant="standard" />
+      </Box>
+
+   <Link to={'/'}>
+   <Stack direction="row" className='submit' spacing={3}>
+         <Button onClick={sendEmail} variant="contained">Submit</Button>
+      </Stack>
+   </Link>
+      </div>
+      <div>
+      <Box
+        className='formwidth'
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '34ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+       
+      </Box>
+
+      </div>
+    </Paper>
+   
+  
+  
+  </>
+  );
+}
